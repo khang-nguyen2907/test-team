c46076ed486e9bb64e31ac9cf38d9312e4db66df
khang-nguyen2907 <giakhang29079@gmail.com>
2023-03-10 03:47:39 +0000

    chore: initialize make dataset

---
+++ src/data/make_dataset.py
@@ -0,0 +1,25 @@
+import json
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+def get_dataset(dataset_name: str,
+                name: str = "japanese") -> datasets.DatasetDict:
+    ds: dataset.DatasetDict
+    return ds
+
+def preprocess(ds: datasets.DatasetDict,
+               tokenizer: AutoTokenizer) -> datasets.DatasetDict:
+    def preprocess_function(example):
+        # Max token size is 14536 and 215 for inputs and labels, espectively.
+        # Here I restrict these token size
+        input_feature = tokenizer(example["text"], truncation=True, max_length=1024)
+        label = tokenizer(example["summary"], truncation=True, max_length=128)
+        return {
+                "input_ids": input_feature["input_ids"],
+                "attention_mask": input_feature["attention_mask"],
+                "labels": label["input_ids"],
+                }
+    tokenized_dataset: datasets.DatasetDict
+    return tokenized_dataset
+
+
